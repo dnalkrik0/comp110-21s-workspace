507189f39cd46ab6fb420ff789236b09a5d69895
Karlisle Kirkland <dnalkrik@ad.unc.edu>
2021-01-25 20:20:37 -0500

    Exercise 00 revised 1

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,2 +1,2 @@
 __author__ = "730391530"
-print("Hello, world. My name is Boberta, and I am a lonely fisherman with no friends except for my aloe vera plant whom I named Jennifer.")
+print("Hello, world. My name is Boberta, and I am a lonely fisherman with no friends.")
